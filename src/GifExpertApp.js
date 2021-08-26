ac616f98bc988d545cfa1c6c1f7d3936934ebe2c
LAMVCL <luismartinez311@outlook.es>
2021-08-26 15:14:02 -0400

    elementos al array con useState y spread operator

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const GifExpertApp = () => {
+
+    
+    //const categories = ['One Punch Man','Full Metal Alchemist','Tokyo Revengers'];
+    const [categories, setCategories] = useState(['One Punch Man','Full Metal Alchemist','Tokyo Revengers'])
+
+    const handleAdd = () =>{
+        const newElement = 'Hunter x Hunter';
+        //Usamos el spread operator para clonar el array y añadir el nuevo elemento
+        setCategories([...categories,newElement]);
+        //Si queremos cambiar el orden del elemento podemos realizar:
+        //setCategories([newElement,...categories]);
+    }    
+
     return (
         <div>
             <h2>GifExpertApp</h2>
             <hr></hr>
+            <ol>
+                {
+                    categories.map( ( category ) =>{
+                        return <li key={ category }> { category } </li>
+                    })
+                }
+            </ol>
+            <button onClick={ handleAdd }>Agregar</button>
         </div>
     )
 }
