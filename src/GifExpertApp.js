50a4674d71ecb5a943859c0632cd5da30a856196
LAMVCL <luismartinez311@outlook.es>
2021-08-26 17:06:43 -0400

    añadimos Proptypes para AddCategory

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -4,25 +4,25 @@ import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
 
-    const [categories, setCategories] = useState(['One Punch Man','Full Metal Alchemist','Tokyo Revengers'])
+    const [categories, setCategories] = useState(['One Punch Man','Full Metal Alchemist','Tokyo Revengers']);
 
     /* const handleAdd = () =>{
         const newElement = 'Hunter x Hunter';
-        //Usamos el spread operator para clonar el array y añadir el nuevo elemento
+        Usamos el spread operator para clonar el array y añadir el nuevo elemento
         setCategories([...categories,newElement]);
-        //Si queremos cambiar el orden del elemento podemos realizar:
-        //setCategories([newElement,...categories]);
+        Si queremos cambiar el orden del elemento podemos realizar:
+        setCategories([newElement,...categories]);
     }   */  
 
     return (
         <div>
             <h2>GifExpertApp</h2>
-            <AddCategory/>
+            <AddCategory setCategories={setCategories}/>
             <hr></hr>
             <ol>
                 {
-                    categories.map( ( category ) =>{
-                        return <li key={ category }> { category } </li>
+                    categories.map((category)=>{
+                        return <li key={category}> {category} </li>
                     })
                 }
             </ol>
--- src/components/AddCategory.js
+++ src/components/AddCategory.js
@@ -1,13 +1,25 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-export const AddCategory = () => {
+//Desestructuramos el prop mediante llaves {}
+export const AddCategory = ({setCategories}) => {
 
     const [inputValue, setInputValue] = useState('');
     const handleInputChange = (e) => {
         setInputValue(e.target.value);
     }
     const handleSubmit = (e) =>{
+        //Bloqueamos la actualización de la página
         e.preventDefault();
+
+        //Validamos que el input posea texto mediante un length
+        if (inputValue.trim().length > 2){
+            //De ser valido asignamos el texto al array mediante setCategories
+            setCategories(newCategorie => [...newCategorie, inputValue]);
+            //Vaciamos el input para evitar duplicados al presionar doble submit
+            setInputValue('');
+        }
+        
     }
     return (
         <form onSubmit={handleSubmit}>
@@ -22,4 +34,8 @@ export const AddCategory = () => {
             <button>enviar!</button>
         </form>
     )
+}
+
+AddCategory.propTypes = {
+    setCategories: PropTypes.func.isRequired
 }
