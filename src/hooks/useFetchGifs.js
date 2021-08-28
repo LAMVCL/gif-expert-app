ad3c8d54d3f9d9c72ac2d1387ad006f740f2c3f2
LAMVCL <luismartinez311@outlook.es>
2021-08-28 04:13:15 -0400

    eliminamos timeout y damos formato

--- src/hooks/useFetchGifs.js
+++ src/hooks/useFetchGifs.js
@@ -3,27 +3,19 @@ import { getGifs } from "../helpers/getGifs";
 //Los hooks son funciones
 
 export const useFetchGifs = (category) => {
-    
+  const [state, setState] = useState({
+    data: [],
+    loading: true,
+  });
 
-    const [state, setState] = useState({
-        data: [],
-        loading: true,
+  useEffect(() => {
+    getGifs(category).then((imgs) => {
+      setState({
+        data: imgs,
+        loading: false,
+      });
     });
+  }, []);
 
-    useEffect(() => {
-
-        getGifs(category).then( imgs => {
-
-            setTimeout(() =>{
-                setState({
-                    data: imgs,
-                    loading: false,
-                })
-            }, 3000);
-
-        });
-
-    }, [])
-
-    return state; // {data: [], loading: true}
-}
+  return state; // {data: [], loading: true}
+};
