abe3e0562e00fe047183a5fdf9fb7119aabf7080
LAMVCL <luismartinez311@outlook.es>
2021-08-28 04:13:39 -0400

    estilización de la web y clases

--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const GifGridItem = ({id, title, url}) => {
 
     return (
-        <div className="card">
+        <div className="card animate__animated animate__fadeInDown">
             <p>{title}</p>
             <img src={url} alt={title}></img>
         </div>
