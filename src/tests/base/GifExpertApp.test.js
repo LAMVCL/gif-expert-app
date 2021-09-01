40581192a14e5aed1532100120a27cb49b3c1e43
LAMVCL <luismartinez311@outlook.es>
2021-08-31 22:47:27 -0400

    más pruebas en gifExpertApp

--- src/tests/base/GifExpertApp.test.js
+++ src/tests/base/GifExpertApp.test.js
@@ -9,4 +9,17 @@ describe('Set de pruebas para GifExpertApp', () => {
         expect(wrapper).toMatchSnapshot();
 
     });
-})
+
+    test('debe de mostrar una lista de categorias ', () => {
+        
+        const categories = ['One punch','Tokyo Revengers'];
+
+        const wrapper = shallow(<GifExpertApp defaultCategories={categories}/>);
+
+        expect(wrapper).toMatchSnapshot();
+        expect(wrapper.find('GifGrid').length).toBe(categories.length);
+    })
+    
+
+
+});
